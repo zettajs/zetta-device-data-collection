e6a85ab266bc63aaa422aad173c123c26bc1ac6f
Adam Magaluk <AdamMagaluK@gmail.com>
2016-06-14 11:48:30 -0400

    Use multiplex streamtopic for subscription when new peer connects

--- collection.js
+++ collection.js
@@ -3,7 +3,15 @@ var url = require('url');
 var util = require('util');
 var rels = require('zetta-rels');
 
-var StatsCollector = module.exports = function() {
+var EXCLUDED_EVENTS = [
+    /^_peer\/.+/, // peer events
+    /^logs$/ // logs topic is emitted for stdout on device transitions
+];
+
+var StatsCollector = module.exports = function(options) {
+  options = options || {};
+  this.includeServerEvents = !!(options.includeServerEvents);
+  
   EventEmitter.call(this);
 };
 util.inherits(StatsCollector, EventEmitter);
@@ -15,45 +23,25 @@ StatsCollector.prototype.collect = function() {
 StatsCollector.prototype._collect = function(runtime) {
   var self = this;
 
+  var peers = [];
   runtime.pubsub.subscribe('_peer/connect', function(ev, msg) {
-    var topic = 'query:collector+' + msg.peer.name + '/where type is not missing';
+    if (peers.indexOf(msg.peer.name) < 0) {
+      msg.peer.subscribe('**');
+      msg.peer.on('zetta-events', function(topic, data) {
+        if (!self.includeServerEvents) {
+          var found = EXCLUDED_EVENTS.some(function(regex) {
+            return regex.test(topic);
+          });
+          
+          if (found) {
+            return;
+          }
+        }
 
-    if (Object.keys(msg.peer.subscriptions).indexOf(encodeURIComponent(topic)) !== -1) {
-      return;
+        self.emit('event', data);
+      });
+      
+      peers.push(msg.peer.name);
     }
-
-    msg.peer.on(topic, function(device) {
-      var links = device.links.filter(function(link) {
-        if (link.rel.indexOf(rels.objectStream) !== -1
-          || link.rel.indexOf(rels.binaryStream) !== -1) {
-          return link;
-        }
-      });
-
-      if (links.length === 0) {
-        return;
-      }
-
-      var topics = links.map(function(link) {
-        var querystring = url.parse(link.href, true);
-        return querystring.query.topic;
-      });
-
-      if (topics.length === 0) {
-        return;
-      }
-
-      topics.forEach(function(topic) {
-        if (Object.keys(msg.peer.subscriptions).indexOf(topic) !== -1) {
-          return;
-        }
-
-        msg.peer.on(topic, function(deviceMessage) {
-          self.emit('event', deviceMessage);
-        });
-        msg.peer.subscribe(topic);
-      });
-    });
-    msg.peer.subscribe(encodeURIComponent(topic));
   });
 };
--- test/test_collection.js
+++ test/test_collection.js
@@ -6,15 +6,16 @@ var Photocell = require('zetta-photocell-mock-driver');
 var StatsCollector = require('../');
 
 describe('StatsCollector', function() {
+  
   it('receives data from devices', function(done) {
     var collector = new StatsCollector();
 
-    cluster = zettacluster({ zetta: zetta })
+    var cluster = zettacluster({ zetta: zetta })
       .server('cloud', [collector.collect()])
       .server('test1', [Photocell], ['cloud']);
 
     cluster.on('ready', function() {
-      collector.on('event', function(msg) {
+      collector.once('event', function(msg) {
         assert(!!msg.data);
         cluster.stop();
         setTimeout(done, 10);
@@ -47,23 +48,21 @@ describe('StatsCollector', function() {
         }, 10);
         return;
       } else if (connectCount === 2) {
-        c1.on('ready', function() {
-          var peer = cloudRuntime.httpServer.peers['test1'];
-          var queryKeys = Object.keys(peer.subscriptions)
+        var peer = cloudRuntime.httpServer.peers['test1'];
+        var queryKeys = Object.keys(peer.subscriptions)
             .filter(function(key) {
-              if (key.slice(0, 5) === 'query') {
+              if (key === '**') {
                 return key;
               }
             });
-          var subscriptionCount = queryKeys.length;
-          assert.equal(subscriptionCount, 1);
+        var subscriptionCount = queryKeys.length;
+        assert.equal(subscriptionCount, 1);
 
-          var queryKey = queryKeys[0];
-          assert.equal(peer.subscriptions[queryKey], 1);
+        var queryKey = queryKeys[0];
+        assert.equal(peer.subscriptions[queryKey], 1);
 
-          c1.stop();
-          setTimeout(done, 10);
-        });
+        c1.stop();
+        setTimeout(done, 10);
       }
     });
 
